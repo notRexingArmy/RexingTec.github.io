48640ad286b23d46dcc11d44a46ee764a8e4b0dd
Li Mao <49698562+RexingTec@users.noreply.github.com>
2023-10-09 18:15:47 +0200

    more list changes

--- js/list.js
+++ js/list.js
@@ -1,6 +1,6 @@
 const d = {
 	"list": [
-	//top 1	/*=================================================================================*/
+	/*=================================================================================*/
 		{
 			"vids": [
 				{
@@ -11,7 +11,7 @@ const d = {
 				},
 			],
 			"name": "Loochiverse",
-                       "author": "DTDom and more",
+                       "author": "DTDom",
                        "more": "Wahffle, Koopazu, Hypno, Nerhy, Azuvy, GiggsRH, Looch, Twisterdude, DTDom",
                        "id": 81373023,
                        "pass": "Free to copy",
@@ -19,7 +19,7 @@ const d = {
                        "verificationVid": "https://www.youtube.com/watch?v=W3dCHPubwvE",
                        "key": 0
 		},
-		//top 2 /*=================================================================================*/
+		/*=================================================================================*/
 		{
 			"vids": [
 				{
@@ -38,7 +38,7 @@ const d = {
                        "verificationVid": "https://www.youtube.com/watch?v=qcxbpny3ORg",
                        "key": 0
 		},
-		//top 3 /*=================================================================================*/
+		/*=================================================================================*/
 		{
 			"vids": [
 				{
@@ -54,10 +54,10 @@ const d = {
                        "id": 78687216,
                        "pass": "Free to copy",
                        "percentToQualify": 58,
-                       "verificationVid": "https://www.youtube.com/watch?v=W3dCHPubwvE",
+                       "verificationVid": "https://www.youtube.com/watch?v=hS8fT0P0MqU",
                        "key": 0
 		},
-		//top 4 /*=================================================================================*/
+		/*=================================================================================*/
 		{
 			"vids": [
 				{
@@ -68,12 +68,50 @@ const d = {
 				},
 			],
 			"name": "Niwa",
-                       "author": "Teno and more",
+                       "author": "Teno",
                        "more": "Mrgm, Aranda, Titoalper, KrazyGFX, Beclan, Asuith, Pedro, Peng, Blaze, Genoxide",
                        "id": 87425029,
                        "pass": "Free to copy",
                        "percentToQualify": 59,
                        "verificationVid": "https://www.youtube.com/watch?v=vegDZr7jh-M",
+                       "key": 0
+		},
+		/*=================================================================================*/
+		{
+			"vids": [
+				{
+					"user": "Li Mao",
+					"link": "https://www.youtube.com/watch?v=XOeVB6Z8Tas",
+					"percent": 100,
+					"hz": "144hz"
+				},
+			],
+			"name": "FISH FISH GODMODE",
+                       "author": "whoiskeve",
+                       "more": "",
+                       "id": 89807521,
+                       "pass": "Free to copy",
+                       "percentToQualify": 73,
+                       "verificationVid": "https://www.youtube.com/watch?v=YMMzsot1w5I&t=1s",
+                       "key": 0
+		},
+		/*=================================================================================*/
+		{
+			"vids": [
+				{
+					"user": "Li Mao",
+					"link": "https://www.youtube.com/watch?v=MoIyGBpe7JU",
+					"percent": 100,
+					"hz": "144hz"
+				},
+			],
+			"name": "HyperSonic",
+                       "author": "Viprin",
+                       "more": "Dudex, Serponge, Manix648, Vlacc, Rustam, nasgubb, ZenthicAlpha, Pan, Evasium, Etzer, Terron",
+                       "id": 30219145,
+                       "pass": "Free to copy",
+                       "percentToQualify": 70,
+                       "verificationVid": "https://www.youtube.com/watch?v=RmrPEJ7Z6lY",
                        "key": 0
 		},
 		
