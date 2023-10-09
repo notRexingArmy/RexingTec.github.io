41c23cc378f6dca542fdbec8003e0f5d38a8e384
Li Mao <49698562+RexingTec@users.noreply.github.com>
2023-10-09 18:05:57 +0200

    added more levels

--- js/list.js
+++ js/list.js
@@ -7,11 +7,11 @@ const d = {
 					"user": "Li Mao",
 					"link": "https://www.youtube.com/watch?v=H0Rk7fTq2Gk",
 					"percent": 100,
-					"hz": "288hz"
+					"hz": "144hz"
 				},
 			],
 			"name": "Loochiverse",
-                       "author": "DTDom",
+                       "author": "DTDom and more",
                        "more": "Wahffle, Koopazu, Hypno, Nerhy, Azuvy, GiggsRH, Looch, Twisterdude, DTDom",
                        "id": 81373023,
                        "pass": "Free to copy",
@@ -20,6 +20,62 @@ const d = {
                        "key": 0
 		},
 		//top 2 /*=================================================================================*/
+		{
+			"vids": [
+				{
+					"user": "Li Mao",
+					"link": "https://www.youtube.com/watch?v=UthcZP1I3mY",
+					"percent": 100,
+					"hz": "240hz"
+				},
+			],
+			"name": "doradura",
+                       "author": "IvashkaUA",
+                       "more": "",
+                       "id": 77114615,
+                       "pass": "Free to copy",
+                       "percentToQualify": 57,
+                       "verificationVid": "https://www.youtube.com/watch?v=qcxbpny3ORg",
+                       "key": 0
+		},
+		//top 3 /*=================================================================================*/
+		{
+			"vids": [
+				{
+					"user": "Li Mao",
+					"link": "https://www.youtube.com/watch?v=zXBM6-7YEDs",
+					"percent": 100,
+					"hz": "144hz"
+				},
+			],
+			"name": "Disruption",
+                       "author": "Ka1sa",
+                       "more": "",
+                       "id": 78687216,
+                       "pass": "Free to copy",
+                       "percentToQualify": 58,
+                       "verificationVid": "https://www.youtube.com/watch?v=W3dCHPubwvE",
+                       "key": 0
+		},
+		//top 4 /*=================================================================================*/
+		{
+			"vids": [
+				{
+					"user": "Li Mao",
+					"link": "https://www.youtube.com/watch?v=XOeVB6Z8Tas",
+					"percent": 100,
+					"hz": "144hz"
+				},
+			],
+			"name": "Niwa",
+                       "author": "Teno and more",
+                       "more": "Mrgm, Aranda, Titoalper, KrazyGFX, Beclan, Asuith, Pedro, Peng, Blaze, Genoxide",
+                       "id": 87425029,
+                       "pass": "Free to copy",
+                       "percentToQualify": 59,
+                       "verificationVid": "https://www.youtube.com/watch?v=vegDZr7jh-M",
+                       "key": 0
+		},
 		
 
 
